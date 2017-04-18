379c003552c0026e67664a26534b9d03e5dd3017
Maxmya <maxmya@outlook.com>
2017-04-19 01:13:05 +0200

    Update

---
+++ initApp.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAF-UfDXWf0z-9fG_V98VI5o8xOcIcuLFo",
+  authDomain: "true-feed.firebaseapp.com",
+  databaseURL: "https://true-feed.firebaseio.com",
+  projectId: "true-feed",
+  storageBucket: "true-feed.appspot.com",
+  messagingSenderId: "68062307547"
+};
+firebase.initializeApp(config);
